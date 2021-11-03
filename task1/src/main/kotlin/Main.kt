5c12a2c0856b66f2eca34ca509a0d4a178c9222a
SmirnovDV-98 <smirnovdv09@gmail.com>
2021-11-03 11:22:25 +0300

    changed capitalize(s:String) realisation. Commented realisation for capitalizing all verbs in string, added capitalize for single string

--- task1/src/main/kotlin/Main.kt
+++ task1/src/main/kotlin/Main.kt
@@ -28,164 +28,169 @@ fun lowCase(s:String):String{// = s.lowercase()
 }
 
 fun capitalize(s:String):String{//=s.capitalize()
-    var res =""
-    val strArr = s.split(" ")
-    for (i in strArr){
-        val chArr:CharArray = i.toCharArray()
-        chArr[0]=chArr[0].uppercaseChar()
-        val iChanged=chArr.concatToString()
-        res="${res}${iChanged} "
-    }
-    return res
+//Capitalize all
+/* var res =""
+ l strArr: List<String> = s.split(" ")
+ for (i in strArr){
+     val chArr:CharArray = i.toCharArray()
+     chArr[0]=chArr[0].uppercaseChar()
+     val iChanged=chArr.concatToString()
+     res="${res}${iChanged} "
+ }
+ return res*/
+ //Just capitalise
+    val chArr:CharArray = s.toCharArray()
+    chArr[0]=chArr[0].uppercaseChar()
+    return chArr.concatToString()
 }
 
 fun compareNoRegister(s1:String, s2:String):String {
-    if (s1.uppercase()==s2.uppercase())
-    {
-        return "They are equal"
-    }
-    val chArr1:CharArray = s1.uppercase().toCharArray()
-    val chArr2:CharArray = s2.uppercase().toCharArray()
+ if (s1.uppercase()==s2.uppercase())
+ {
+     return "They are equal"
+ }
+ val chArr1:CharArray = s1.uppercase().toCharArray()
+ val chArr2:CharArray = s2.uppercase().toCharArray()
 
-    var val1 =0
-    var val2 =0
+ var val1 =0
+ var val2 =0
 
-    for (i in chArr1){
-        val1+=i.code
-    }
-    for (i in chArr2){
-        val2+=i.code
-    }
-    return if (val1>val2){
-        "$s1 is bigger then $s2"
-    } else {
-        "$s2 is bigger then $s1"
-    }
+ for (i in chArr1){
+     val1+=i.code
+ }
+ for (i in chArr2){
+     val2+=i.code
+ }
+ return if (val1>val2){
+     "$s1 is bigger then $s2"
+ } else {
+     "$s2 is bigger then $s1"
+ }
 }
 
 fun compareWithRegister(s1:String, s2:String):String{
-    if (s1==s2)
-    {
-        return "They are equal"
-    }
-    val chArr1:CharArray = s1.toCharArray()
-    val chArr2:CharArray = s2.toCharArray()
+ if (s1==s2)
+ {
+     return "They are equal"
+ }
+ val chArr1:CharArray = s1.toCharArray()
+ val chArr2:CharArray = s2.toCharArray()
 
-    var val1 =0
-    var val2 =0
+ var val1 =0
+ var val2 =0
 
-    for (i in chArr1){
-        val1+=i.code
-    }
-    for (i in chArr2){
-        val2+=i.code
-    }
-    println("$val1 $val2")
-    return if (val1>val2){
-        "$s1 is bigger then $s2"
-    } else {
-        "$s2 is bigger then $s1"
-    }
+ for (i in chArr1){
+     val1+=i.code
+ }
+ for (i in chArr2){
+     val2+=i.code
+ }
+ println("$val1 $val2")
+ return if (val1>val2){
+     "$s1 is bigger then $s2"
+ } else {
+     "$s2 is bigger then $s1"
+ }
 }
 
 fun subStringSearchFromPosition(s:String,subString:String, n:Int):Boolean{
-    if ((n<1||n>s.length)){
-        throw(CustomException())
-    }
-    if ((s==subString)&&(n==0)){
-        return true
-    } else if (s==subString){
-        return false
-    }
-    val sToCharArr=s.toCharArray(n-1)
-    for (i in 0..sToCharArr.size-1-(subString.length-1)){
-        var counter =0
-        for (j in subString.indices){
-            if (sToCharArr[i+j]==subString[j]){
-                counter++
-            }
-        }
-        if (counter==subString.length){
-            return true
-        }
-    }
-    return false
+ if ((n<1||n>s.length)){
+     throw(CustomException())
+ }
+ if ((s==subString)&&(n==0)){
+     return true
+ } else if (s==subString){
+     return false
+ }
+ val sToCharArr=s.toCharArray(n-1)
+ for (i in 0..sToCharArr.size-1-(subString.length-1)){
+     var counter =0
+     for (j in subString.indices){
+         if (sToCharArr[i+j]==subString[j]){
+             counter++
+         }
+     }
+     if (counter==subString.length){
+         return true
+     }
+ }
+ return false
 }
 
 fun registerCheck(s:String){
-    when(s){
-        s.uppercase()->{
-            println("String $s in UPPERCASE")
-        }
-        s.lowercase()->{
-            println("String $s in lowercase")
-        }
-        s.replaceFirstChar { it.uppercase() }->{
-            println("String $s is capitalized")
-        }
-        else->{
-            println("String $s is something else")
-        }
-    }
+ when(s){
+     s.uppercase()->{
+         println("String $s in UPPERCASE")
+     }
+     s.lowercase()->{
+         println("String $s in lowercase")
+     }
+     s.replaceFirstChar { it.uppercase() }->{
+         println("String $s is capitalized")
+     }
+     else->{
+         println("String $s is something else")
+     }
+ }
 }
 /**
- * Functions, used for 1B part
- * main of them - changeText, others used inside
- */
+* Functions, used for 1B part
+* main of them - changeText, others used inside
+*/
 fun writeIt(path:String, strList:List<String>){
-    File(path).printWriter().use { out -> strList.forEach { out.println(it) } }
+ File(path).printWriter().use { out -> strList.forEach { out.println(it) } }
 }
 
 fun readIt(pathToRead:String):List<String>{
-    val lineList = mutableListOf<String>()
-    File(pathToRead).useLines { lines -> lines.forEach { lineList.add(it) }}
-    return lineList.toList()
+ val lineList = mutableListOf<String>()
+ File(pathToRead).useLines { lines -> lines.forEach { lineList.add(it) }}
+ return lineList.toList()
 }
 
 fun multiSplit(s:String):List<String>{
-    val reg = Regex("(?<=[!? .,:;-])|(?=[!? .,:;-])")
-    return s.split(reg)
+ val reg = Regex("(?<=[!? .,:;-])|(?=[!? .,:;-])")
+ return s.split(reg)
 }
 
 fun changeText(pathToRead:String, pathToWrite:String){
-    val list = readIt(pathToRead)
-    val resStrListFin = mutableListOf<String>()
-    for (i in list) {
-        var resStrList =""
-        val stringListToWorkWith = multiSplit(i)
-        var flag =true
-        for (j in stringListToWorkWith) {
-            if ((j == "!") || (j == "?") || (j == ".")) {
-                flag = true
-            }
-            if ((j==".")||(j==",")||(j=="?")||(j=="!")||(j==" ")||(j==":")||(j==";")||(j=="-")){
-                resStrList="$resStrList$j"
-                continue
-            }
-            if (j==j.uppercase()){
-                resStrList="$resStrList$j"
-                flag=false
-                continue
-            }
-            if (j==j.lowercase().replaceFirstChar { it.uppercase() }){
-                resStrList="$resStrList$j"
-                flag=false
-                continue
-            }
-            if (flag){
-                resStrList="$resStrList${j.lowercase().replaceFirstChar { it.uppercase() }}"
-                flag=false
-                continue
-            } else {
-                // resStrList.add(j.lowercase())
-                resStrList="$resStrList${j.lowercase()}"
-                flag=false
-                continue
-            }
-        }
-        resStrListFin.add(resStrList)
-    }
-    writeIt(pathToWrite,resStrListFin)
+ val list = readIt(pathToRead)
+ val resStrListFin = mutableListOf<String>()
+ for (i in list) {
+     var resStrList =""
+     val stringListToWorkWith = multiSplit(i)
+     var flag =true
+     for (j in stringListToWorkWith) {
+         if ((j == "!") || (j == "?") || (j == ".")) {
+             flag = true
+         }
+         if ((j==".")||(j==",")||(j=="?")||(j=="!")||(j==" ")||(j==":")||(j==";")||(j=="-")){
+             resStrList="$resStrList$j"
+             continue
+         }
+         if (j==j.uppercase()){
+             resStrList="$resStrList$j"
+             flag=false
+             continue
+         }
+         if (j==j.lowercase().replaceFirstChar { it.uppercase() }){
+             resStrList="$resStrList$j"
+             flag=false
+             continue
+         }
+         if (flag){
+             resStrList="$resStrList${j.lowercase().replaceFirstChar { it.uppercase() }}"
+             flag=false
+             continue
+         } else {
+             // resStrList.add(j.lowercase())
+             resStrList="$resStrList${j.lowercase()}"
+             flag=false
+             continue
+         }
+     }
+     resStrListFin.add(resStrList)
+ }
+ writeIt(pathToWrite,resStrListFin)
 }
 
 class CustomException :Exception()
